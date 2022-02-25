9818fbfde8a861b392e4aeff5eeafd6107b52f44
nadiaf85 <nadia.fiorio@alice.it>
2022-02-25 15:07:13 +0100

    push

---
+++ classes/movie.php
@@ -0,0 +1,18 @@
+<?php
+
+class Movie{
+    private $titolo;
+    private $genere;
+    private $anno;
+
+    public function __construct($_titolo, $_genere, $_anno){
+        $this->titolo = $_titolo;
+        $this->genere = $_genere;
+        $this->anno = $_anno;
+    }
+
+    public function getInfo(){
+        return {$this->titolo} {$this->genere} {$this->anno};
+    }
+}
+?>
--- index.php
+++ index.php
@@ -5,3 +5,42 @@ metodi d’istanza che abbiamo visto stamattina e create un file index.php in cu
    => all’interno della classe è definito un costruttore
    => all’interno della classe è definito almeno un metodo
 - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
+
+
+<?php
+
+require_once __DIR__."./classes/Movie.php";
+
+$Film1 = [
+    new Movie ("ritorno al futuro 1","fantascienza","1985"),
+    new Movie ("ritorno al futuro 2","fantascienza","1989"),
+    new Movie ("ritorno al futuro 3","fantascienza","1990"),
+];
+
+
+var_dump($Film1)
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<ul>
+<?php
+
+    foreach ($Film1 as $elemento){
+        echo "<li> {$elemento->getInfo()} </li>"
+    }
+?>
+</ul>
+
+</body>
+</html>
